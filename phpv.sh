fac173e92dc64c2d570354870d32412c256b8c39
Supan Adit Pratama <email@supanadit.com>
2025-10-25 16:39:11 +0700

    refactor: update directory variables for better clarity and organization

--- phpv.sh
+++ phpv.sh
@@ -5,35 +5,37 @@
 # Manages multiple PHP versions in user space
 
 # Source all script modules
-PHPV_DRIVER_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/drivers" && pwd)"
-PHPV_LIB_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/lib" && pwd)"
-PHPV_BIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/bin" && pwd)"
+PHPV_SCRIPT_DRIVER_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/drivers" && pwd)"
+PHPV_SCRIPT_DEPENDENCY_DIR="$PHPV_SCRIPT_DRIVER_DIR/dependencies"
 
-source "$PHPV_LIB_DIR/common.sh"
-source "$PHPV_LIB_DIR/build.sh"
+PHPV_SCRIPT_LIB_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/lib" && pwd)"
+PHPV_SCRIPT_BIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/bin" && pwd)"
+
+source "$PHPV_SCRIPT_LIB_DIR/common.sh"
+source "$PHPV_SCRIPT_LIB_DIR/build.sh"
 
 # Dependencies
-source "$PHPV_DRIVER_DIR/dependencies/cmake.sh"
-source "$PHPV_DRIVER_DIR/dependencies/curl.sh"
-source "$PHPV_DRIVER_DIR/dependencies/freetype.sh"
-source "$PHPV_DRIVER_DIR/dependencies/icu.sh"
-source "$PHPV_DRIVER_DIR/dependencies/libjpeg.sh"
-source "$PHPV_DRIVER_DIR/dependencies/libpng.sh"
-source "$PHPV_DRIVER_DIR/dependencies/libxml2.sh"
-source "$PHPV_DRIVER_DIR/dependencies/libzip.sh"
-source "$PHPV_DRIVER_DIR/dependencies/mariadb.sh"
-source "$PHPV_DRIVER_DIR/dependencies/mysql.sh"
-source "$PHPV_DRIVER_DIR/dependencies/odbc.sh"
-source "$PHPV_DRIVER_DIR/dependencies/oniguruma.sh"
-source "$PHPV_DRIVER_DIR/dependencies/openssl.sh"
-source "$PHPV_DRIVER_DIR/dependencies/postgresql.sh"
-source "$PHPV_DRIVER_DIR/dependencies/zlib.sh"
+source "$PHPV_SCRIPT_DEPENDENCY_DIR/cmake.sh"
+source "$PHPV_SCRIPT_DEPENDENCY_DIR/curl.sh"
+source "$PHPV_SCRIPT_DEPENDENCY_DIR/freetype.sh"
+source "$PHPV_SCRIPT_DEPENDENCY_DIR/icu.sh"
+source "$PHPV_SCRIPT_DEPENDENCY_DIR/libjpeg.sh"
+source "$PHPV_SCRIPT_DEPENDENCY_DIR/libpng.sh"
+source "$PHPV_SCRIPT_DEPENDENCY_DIR/libxml2.sh"
+source "$PHPV_SCRIPT_DEPENDENCY_DIR/libzip.sh"
+source "$PHPV_SCRIPT_DEPENDENCY_DIR/mariadb.sh"
+source "$PHPV_SCRIPT_DEPENDENCY_DIR/mysql.sh"
+source "$PHPV_SCRIPT_DEPENDENCY_DIR/odbc.sh"
+source "$PHPV_SCRIPT_DEPENDENCY_DIR/oniguruma.sh"
+source "$PHPV_SCRIPT_DEPENDENCY_DIR/openssl.sh"
+source "$PHPV_SCRIPT_DEPENDENCY_DIR/postgresql.sh"
+source "$PHPV_SCRIPT_DEPENDENCY_DIR/zlib.sh"
 
 
-source "$PHPV_LIB_DIR/versions.sh"
+source "$PHPV_SCRIPT_LIB_DIR/versions.sh"
 source "$PHPV_DRIVER_DIR/llvm.sh"
-source "$PHPV_LIB_DIR/commands.sh"
-source "$PHPV_BIN_DIR/main.sh"
+source "$PHPV_SCRIPT_LIB_DIR/commands.sh"
+source "$PHPV_SCRIPT_BIN_DIR/main.sh"
 
 # Run main function if script is executed directly
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then

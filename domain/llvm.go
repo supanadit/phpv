f6b33b9b22023831e74e86f57a0f535f0d2b72c7
Supan Adit Pratama <email@supanadit.com>
2025-11-22 13:25:48 +0700

    feat: update LLVM download URLs for PHP version compatibility

--- domain/llvm.go
+++ domain/llvm.go
@@ -20,15 +20,15 @@ func GetLLVMVersionForPHP(phpVersion Version) LLVMVersion {
 	if phpVersion.Major == 8 {
 		return LLVMVersion{
 			Version:     "18.1.8",
-			DownloadURL: "https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.8/LLVM-18.1.8-Linux-X64.tar.xz",
+			DownloadURL: "https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.8/clang+llvm-18.1.8-x86_64-linux-gnu-ubuntu-18.04.tar.xz",
 		}
 	}
 
 	// PHP 7.4 - Use LLVM 15
 	if phpVersion.Major == 7 && phpVersion.Minor == 4 {
 		return LLVMVersion{
-			Version:     "15.0.7",
-			DownloadURL: "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/LLVM-15.0.7-Linux-X64.tar.xz",
+			Version:     "15.0.6",
+			DownloadURL: "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.6/clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar.xz",
 		}
 	}
 
@@ -36,7 +36,7 @@ func GetLLVMVersionForPHP(phpVersion Version) LLVMVersion {
 	if phpVersion.Major == 7 {
 		return LLVMVersion{
 			Version:     "12.0.1",
-			DownloadURL: "https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.1/LLVM-12.0.1-Linux-X64.tar.xz",
+			DownloadURL: "https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.1/clang+llvm-12.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz",
 		}
 	}
 
@@ -44,7 +44,7 @@ func GetLLVMVersionForPHP(phpVersion Version) LLVMVersion {
 	if phpVersion.Major == 5 {
 		return LLVMVersion{
 			Version:     "8.0.1",
-			DownloadURL: "https://github.com/llvm/llvm-project/releases/download/llvmorg-8.0.1/LLVM-8.0.1-Linux-X64.tar.xz",
+			DownloadURL: "https://github.com/llvm/llvm-project/releases/download/llvmorg-8.0.1/clang+llvm-8.0.1-x86_64-linux-gnu-ubuntu-14.04.tar.xz",
 		}
 	}
 

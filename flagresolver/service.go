32f146e253d4de543849ea49d1b4c70d02a7f7d2
Supan Adit Pratama <email@supanadit.com>
2026-04-11 22:11:31 +0700

    refactor: merge flagresolver repository.go into service.go

--- flagresolver/service.go
+++ flagresolver/service.go
@@ -3,11 +3,22 @@ package flagresolver
 import (
 	"errors"
 
+	"github.com/supanadit/phpv/domain"
 	"github.com/supanadit/phpv/internal/utils"
 )
 
 var ErrUnknownExtension = errors.New("unknown extension")
 var ErrExtensionConflict = errors.New("extension conflict")
+
+type Repository interface {
+	GetExtensionDef(name string) (domain.ExtensionDef, bool)
+	IsExtensionValidForPHPVersion(name string, phpVersion string) bool
+	GetConflictingExtensions(name string) []string
+	GetExtensionDependency(name string) (string, bool)
+	GetExtensionDependencyWithVersion(extName, phpVersion string) (string, string, bool)
+	ValidateExtensions(extensions []string, phpVersion string) ([]string, error)
+	CheckExtensionConflicts(extensions []string) ([]string, [][]string)
+}
 
 type Service struct {
 	repo Repository

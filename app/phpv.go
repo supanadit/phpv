536e9e7c1abbbe9d4fa8aab24e61aa28fabd12f4
Supan Adit Pratama <email@supanadit.com>
2026-07-11 12:09:45 +0700

    feat: beautiful wiring by uber fx

--- app/phpv.go
+++ app/phpv.go
@@ -5,29 +5,37 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"go.uber.org/fx"
 
 	"github.com/supanadit/phpv/internal/repository/disk"
 	"github.com/supanadit/phpv/internal/repository/memory"
 	"github.com/supanadit/phpv/internal/terminal"
+	"github.com/supanadit/phpv/registry"
 	"github.com/supanadit/phpv/silo"
 )
 
 func main() {
-	// Prepare repositories
-	registryRepo := memory.NewRegistryRepository()
-	siloRepo := disk.NewSiloRepository()
-
-	// Build service layer
-	siloService := silo.NewService(siloRepo, registryRepo)
-
-	// Prepare delivery layer (cobra)
 	rootCmd := &cobra.Command{
 		Use:   "phpv",
 		Short: "PHP version manager",
 	}
-	terminal.NewPHPHandler(rootCmd, siloService)
 
-	// Execute
+	app := fx.New(
+		fx.NopLogger,
+		fx.Provide(
+			func() *cobra.Command { return rootCmd },
+			fx.Annotate(memory.NewRegistryRepository, fx.As(new(registry.RegistryRepository))),
+			fx.Annotate(disk.NewSiloRepository, fx.As(new(silo.SiloRepository))),
+			fx.Annotate(silo.NewService, fx.As(new(terminal.PHPService))),
+		),
+		fx.Invoke(terminal.NewPHPHandler),
+	)
+
+	if err := app.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
